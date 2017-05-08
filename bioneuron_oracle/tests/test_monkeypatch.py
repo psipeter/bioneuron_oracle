54d3d85ecfba7a6f142bcc6bf9e0a6dfd4f40bcf
Aaron Voelker <arvoelke@gmail.com>
2017-05-08 09:30:42 -0400

    Another monkeypatch test

--- bioneuron_oracle/tests/test_monkeypatch.py
+++ bioneuron_oracle/tests/test_monkeypatch.py
@@ -1,11 +1,12 @@
 import nengo
+from nengo.utils.testing import warns
 
 from bioneuron_oracle import patch, unpatch, BioConnection, BioSimulator
 from bioneuron_oracle.monkeypatch import NengoConnection
 from bioneuron_oracle.monkeypatch import NengoSimulator
 
 
-def test_monkepatching():
+def test_monkeypatching():
     assert BioConnection is not NengoConnection
     assert BioSimulator is not NengoSimulator
 
@@ -17,5 +18,21 @@ def test_monkepatching():
     assert nengo.Simulator is NengoSimulator
 
     patch()
+    assert nengo.Connection is BioConnection
+    assert nengo.Simulator is BioSimulator
+
+
+def test_repeated_monkeypatching():
+    unpatch()
+    with warns(UserWarning):
+        unpatch()
+
+    assert nengo.Connection is NengoConnection
+    assert nengo.Simulator is NengoSimulator
+
+    patch()
+    with warns(UserWarning):
+        patch()
+
     assert nengo.Connection is BioConnection
     assert nengo.Simulator is BioSimulator
